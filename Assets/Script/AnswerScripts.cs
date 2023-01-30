6f75aadf89891bb961dd2391ca31811a1f22e118
godnine <godninegamer@gmail.com>
2023-01-30 17:09:34 +0700

    Edit AnswerScripts set effect

--- Assets/Script/AnswerScripts.cs
+++ Assets/Script/AnswerScripts.cs
@@ -6,17 +6,23 @@ namespace Script {
     {
         public bool isCorrect = false;
         public QizeManager qize;
+
+        [Header("Effect")]
+        public GameObject correctFx;
+        public GameObject wrongFx;
         public void Answer()
         {
             if (isCorrect)
             {
                 Debug.Log("isCorrect");
                 qize.Corect();
+                correctFx.SetActive(true);
                 GameManager.scoreNum += 1;
             }
             else
             {
                 Debug.Log("isNotCorrect");
+                wrongFx.SetActive(true);
                 qize.Corect();
                 //GameManager.scoreNum -= 1;
             }
